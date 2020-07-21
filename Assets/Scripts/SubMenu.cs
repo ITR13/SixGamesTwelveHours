44f12bb808483b801297d79ba6485b1e19b407d6
ITR <ITR@live.no>
2020-07-21 20:12:00 +0200

    01:32

--- Assets/Scripts/SubMenu.cs
+++ Assets/Scripts/SubMenu.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SubMenu : MonoBehaviour
 {
     [SerializeField] private Button Retry;
-    [SerializeField] private 
+    [SerializeField] private Button MainMenu;
+
+    private void Awake()
+    {
+        Retry.onClick.AddListener(
+            () => SceneManager.LoadScene(
+                SceneManager.GetActiveScene().buildIndex
+            )
+        );
+        MainMenu.onClick.AddListener();
+    }
 }

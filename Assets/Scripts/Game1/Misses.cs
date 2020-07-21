b41d56a26d9a49ce40e4264c8ae5f19d6c37884c
ITR <ITR@live.no>
2020-07-21 19:54:00 +0200

    01:14

--- Assets/Scripts/Game1/Misses.cs
+++ Assets/Scripts/Game1/Misses.cs
@@ -1,9 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 namespace Game1
 {
     public class Misses : MonoBehaviour
     {
-        private static Misses Misses;
+        private static Misses _misses;
+        private int _count;
+        private TextMeshProUGUI _text;
+
+        private void Awake()
+        {
+            _misses = this;
+            _count = 0;
+            _text = GetComponent<TextMeshProUGUI>();
+            _text.text = "";
+        }
+
+        public static void 
     }
 }

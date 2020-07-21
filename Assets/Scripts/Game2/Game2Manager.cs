0e9f861fe11414ce21c6b5cba5a5568ec1ae1b4e
ITR <ITR@live.no>
2020-07-21 22:16:00 +0200

    03:36

--- Assets/Scripts/Game2/Game2Manager.cs
+++ Assets/Scripts/Game2/Game2Manager.cs
@@ -71,6 +71,7 @@ namespace Game2
             TotalError = 0;
 
             highscoreText.text = Highscore.ToString();
+            subMenu.SetActive(false);
         }
 
         private void Update()
@@ -164,7 +165,7 @@ namespace Game2
         private void PlayBeep()
         {
             var length = Mathf.FloorToInt(Mathf.Lerp(0.3f, 0.75f, TotalError) * Bps);
-            var frequency = 440 * Mathf.Pow()
+            var frequency = 440 * Mathf.Pow(2, TotalError * 10);
             var clip = AudioClip.Create("beep", length, 1, Bps, false);
             clip.SetData(
                 GenerateAudio(

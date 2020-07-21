c2751401e9c3366536d6299f9e5d4b1dd8f2968a
ITR <ITR@live.no>
2020-07-21 20:45:00 +0200

    02:05

--- Assets/Scripts/Game2/Game2Manager.cs
+++ Assets/Scripts/Game2/Game2Manager.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Game2Manager : MonoBehaviour
+namespace Game2
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Game2Manager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Image circle;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float _gameTime;
+
+        private void Awake()
+        {
+            _gameTime = -3;
+        }
+
+        private void Awake()
+        {
+            _gameTime
+        }
     }
 }

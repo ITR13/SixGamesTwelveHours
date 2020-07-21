f78583c32e5ba107e872e8158be87fd7478ff044
ITR <ITR@live.no>
2020-07-22 01:34:00 +0200

    06:24

--- Assets/Scripts/Game4/HackAndSlasher.cs
+++ Assets/Scripts/Game4/HackAndSlasher.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Game4
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HackAndSlasher : MonoBehaviour
     {
-        
-    }
+        private Rigidbody2D rigidBody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private Vector2 Movement;
+
+        private  
     }
 }

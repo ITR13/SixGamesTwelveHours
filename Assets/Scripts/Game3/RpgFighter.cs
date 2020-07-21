421b6c5bfb769f2abd7a5ddfe23375d67f4eef56
ITR <ITR@live.no>
2020-07-21 23:08:00 +0200

    04:28

--- Assets/Scripts/Game3/RpgFighter.cs
+++ Assets/Scripts/Game3/RpgFighter.cs
@@ -6,6 +6,11 @@ namespace Game3
     public class RpgFighter : MonoBehaviour
     {
         [SerializeField] private Image sprite;
+        [SerializeField] private Image healthBar;
+        [SerializeField] private Image energyBar;
 
+        [SerializeField] private int totalEnergy;
+
+        public float Health => 
     }
 }

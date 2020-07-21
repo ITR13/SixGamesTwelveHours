a7bd0e033c9825b8525adf63115b328aeafeb4b6
ITR <ITR@live.no>
2020-07-21 22:04:00 +0200

    03:24

--- Assets/Scripts/Game2/Game2Manager.cs
+++ Assets/Scripts/Game2/Game2Manager.cs
@@ -152,7 +152,6 @@ namespace Game2
                 0
             );
             audioSource.clip = clip;
-            SaveAudioClipToWav(clip, "./audio.wav");
             audioSource.Play();
         }
 
@@ -179,45 +178,7 @@ namespace Game2
                 data[i + preFalloff] = h * scale;
             }
 
-            var byteArray = new byte[data.Length * 4];
-            Buffer.BlockCopy(data, 0, byteArray, 0, byteArray.Length);
-            File.WriteAllBytes("./audio", byteArray);
-
             return data;
-        }
-
-        static public void SaveAudioClipToWav(AudioClip audioClip, string filename)
-        {
-            FileStream fsWrite = File.Open(filename, FileMode.Create);
-
-            BinaryWriter bw = new BinaryWriter(fsWrite);
-
-            Byte[] header = { 82, 73, 70, 70, 22, 10, 4, 0, 87, 65, 86, 69, 102, 109, 116, 32 };
-            bw.Write(header);
-
-            Byte[] header2 = { 16, 0, 0, 0, 1, 0, 1, 0, 68, 172, 0, 0, 136, 88, 1, 0 };
-            bw.Write(header2);
-
-            Byte[] header3 = { 2, 0, 16, 0, 100, 97, 116, 97, 152, 9, 4, 0 };
-            bw.Write(header3);
-
-            float[] samples = new float[audioClip.samples];
-            audioClip.GetData(samples, 0);
-            int i = 0;
-
-            while (i < audioClip.samples)
-            {
-                int sampleInt = (int)(32000.0 * samples[i++]);
-
-                int msb = sampleInt / 256;
-                int lsb = sampleInt - (msb * 256);
-
-                bw.Write((Byte)lsb);
-                bw.Write((Byte)msb);
-            }
-
-            fsWrite.Close();
-
         }
     }
 }

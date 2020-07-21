8f084204d735bd73474b6bf35cc336ef3987f982
ITR <ITR@live.no>
2020-07-21 18:44:00 +0200

    00:04

--- Assets/Scripts/Game1/Click.cs
+++ Assets/Scripts/Game1/Click.cs
@@ -1,23 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickTest : MonoBehaviour
+namespace Game1
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Click : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
+        }
 
-    private void OnMouseDown()
-    {
-        Debug.Log("Test!");
+        private void OnMouseDown()
+        {
+            Debug.Log("Test!");
+        }
     }
 }

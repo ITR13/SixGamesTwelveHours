7071074184f34ed6ee8968484c2a58ad22035e1a
ITR <ITR@live.no>
2020-07-21 18:54:00 +0200

    00:14

--- Assets/Scripts/Game1/Game1Manager.cs
+++ Assets/Scripts/Game1/Game1Manager.cs
@@ -7,13 +7,26 @@ namespace Game1
         public static float Time { get; private set; }
         public static int Lives;
 
+        private static int _prevLives;
+
         private void Awake()
         {
             Time = -3f;
+            Lives = 3;
+            _prevLives = Lives;
         }
 
         private void Update()
         {
+            if (Lives != _prevLives)
+            {
+                UpdateLives();
+            }
+        }
+
+        private void UpdateLives()
+        {
+            _prevLives = Lives;
 
         }
     }

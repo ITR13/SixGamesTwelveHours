69434ca19881d3585663c921885ebb21820e445a
ITR <ITR@live.no>
2020-07-22 02:30:00 +0200

    07:20

--- Assets/Scripts/Game4/HasEnemy.cs
+++ Assets/Scripts/Game4/HasEnemy.cs
@@ -4,6 +4,7 @@ namespace Game4
 {
     public class HasEnemy : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private Rigidbody2D rigidBody;
         [SerializeField] private int health;
 
@@ -18,12 +19,26 @@ namespace Game4
 
         public void HurtMe(Vector3 direction)
         {
+            if (_invulnerable > 0) return;
             rigidBody.AddForce(
                 direction.normalized * 8,
                 ForceMode2D.Impulse
             );
             health -= 1;
-            _invulnerable = 0.5f;
+            _invulnerable = 1.5f;
+        }
+
+        private void Update()
+        {
+            _invulnerable -= Time.deltaTime;
+            spriteRenderer.color = new Color(
+                1,
+                1,
+                1,
+                Mathf.Clamp01(0.5f - _invulnerable) + 0.5f
+            );
+            spriteRenderer.enabled =
+                _invulnerable <= 0.5f || (_invulnerable % 0.4f > 0.15f);
         }
     }
 }

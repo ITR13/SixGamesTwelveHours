7d0abf89beeb035b8e1d9eab96d902fbe77a37bd
ITR <ITR@live.no>
2020-07-22 03:39:00 +0200

    08:29

--- Assets/Scripts/Game5/Manager.cs
+++ Assets/Scripts/Game5/Manager.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager : MonoBehaviour
+namespace Game5
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Manager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Row[] rows;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

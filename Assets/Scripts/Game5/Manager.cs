4507779b8ef52e402b8a1c086aed022d943b2af6
ITR <ITR@live.no>
2020-07-22 03:46:00 +0200

    08:36

--- Assets/Scripts/Game5/Manager.cs
+++ Assets/Scripts/Game5/Manager.cs
@@ -18,11 +18,18 @@ namespace Game5
         [SerializeField] private Row[] rows;
 
         private float _time;
+        private int _beat;
 
         private void Update()
         {
             _time += Time.deltaTime / BeatsPerSecond;
+            if(_time < _beat) return;
+            foreach (var row in rows)
+            {
+                row.Play(_beat);
+            }
 
+            _beat++;
         }
     }
 }

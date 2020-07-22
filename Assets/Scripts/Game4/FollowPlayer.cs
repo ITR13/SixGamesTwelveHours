a75bfb28fb093036304550799df19445d6ce305f
ITR <ITR@live.no>
2020-07-22 02:35:00 +0200

    07:25

--- Assets/Scripts/Game4/FollowPlayer.cs
+++ Assets/Scripts/Game4/FollowPlayer.cs
@@ -14,10 +14,10 @@ public class FollowPlayer : MonoBehaviour
 
     private void LateUpdate()
     {
-        var mousePos = (Input.mousePosition.x / (float)Screen.width) - 0.5f;
-        mousePos = Mathf.Clamp01(mousePos * 2 * 6);
+        /*var mousePos = (Input.mousePosition.x / (float)Screen.width) - 0.5f;
+        mousePos = Mathf.Clamp01(mousePos * 2 * 6);*/
         var targetPos = transform.position;
-        targetPos.x = target.transform.position.x + _offset * mousePos;
+        targetPos.x = target.transform.position.x + _offset; // * mousePos;
         transform.position = targetPos;
     }
 }

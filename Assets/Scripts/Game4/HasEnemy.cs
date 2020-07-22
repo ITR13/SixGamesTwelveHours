edce05ad91b222cda4b8c7075cbb270f094904fb
ITR <ITR@live.no>
2020-07-22 02:28:00 +0200

    07:18

--- Assets/Scripts/Game4/HasEnemy.cs
+++ Assets/Scripts/Game4/HasEnemy.cs
@@ -4,11 +4,20 @@ namespace Game4
 {
     public class HasEnemy : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D rigidBody;
+
+        private void 
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             var player = other.gameObject.GetComponent<HackAndSlasher>();
             if (player == null) return;
             player.HurtMe();
+        }
+
+        public void HurtMe(Vector3 direction)
+        {
+            
         }
     }
 }

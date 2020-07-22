07f3b7f119e96cfb57c164418f65643190128c2f
ITR <ITR@live.no>
2020-07-22 05:21:00 +0200

    10:11

--- Assets/Scripts/Game6/TtsPlayer.cs
+++ Assets/Scripts/Game6/TtsPlayer.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class TtsPlayer : MonoBehaviour
 {
     [SerializeField] private new Camera camera;
-    [SerializeField] private 
+
+
+    private void Update()
+    {
+        var worldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 }

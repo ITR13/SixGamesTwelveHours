d5f9eafdf7df1c5c7b8c04f7cc287856f81cbff9
ITR <ITR@live.no>
2020-07-22 05:22:00 +0200

    10:12

--- Assets/Scripts/Game6/TtsPlayer.cs
+++ Assets/Scripts/Game6/TtsPlayer.cs
@@ -7,10 +7,12 @@ public class TtsPlayer : MonoBehaviour
 {
     [SerializeField] private new Camera camera;
 
+    private Vector2 movementDir;
 
     private void Update()
     {
         var worldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+        movementDir = transform.position - worldPoint;
     }
 
     private void FixedUpdate()

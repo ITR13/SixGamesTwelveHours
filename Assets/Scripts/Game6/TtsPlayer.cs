c098276cd3b30f31ff60bb821aff620c7b3a81b0
ITR <ITR@live.no>
2020-07-22 05:31:00 +0200

    10:21

--- Assets/Scripts/Game6/TtsPlayer.cs
+++ Assets/Scripts/Game6/TtsPlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine.PlayerLoop;
 public class TtsPlayer : MonoBehaviour
 {
     [SerializeField] private new Camera camera;
+    [SerializeField] private GameObject bullet;
 
     private Vector3 movementDir;
     private Vector3 shootDir;

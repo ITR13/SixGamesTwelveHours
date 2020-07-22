e0b5948fb408214b5990bd516fb7b0b954fe1368
ITR <ITR@live.no>
2020-07-22 02:29:00 +0200

    07:19

--- Assets/Scripts/Game4/HasEnemy.cs
+++ Assets/Scripts/Game4/HasEnemy.cs
@@ -5,8 +5,9 @@ namespace Game4
     public class HasEnemy : MonoBehaviour
     {
         [SerializeField] private Rigidbody2D rigidBody;
+        [SerializeField] private int health;
 
-        private void 
+        private float _invulnerable;
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -17,7 +18,12 @@ namespace Game4
 
         public void HurtMe(Vector3 direction)
         {
-            
+            rigidBody.AddForce(
+                direction.normalized * 8,
+                ForceMode2D.Impulse
+            );
+            health -= 1;
+            _invulnerable = 0.5f;
         }
     }
 }

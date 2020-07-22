86b6f4a7b0fa9e232d9759c6eadc38d29d490770
ITR <ITR@live.no>
2020-07-22 06:11:00 +0200

    11:01

--- Assets/Scripts/Game6/SavePoint.cs
+++ Assets/Scripts/Game6/SavePoint.cs
@@ -1,11 +1,27 @@
+using System.Linq.Expressions;
 using UnityEngine;
 
 namespace Game6
 {
     public class SavePoint : MonoBehaviour
     {
+        private static readonly Color OffColor = new Color(
+            0,
+            0.7592501f,
+            0.9622642f
+        );
+        private static readonly Color OnColor = new Color(
+            0,
+            0.7592501f,
+            0.9622642f
+        );
+
+
         [SerializeField] private SpriteRenderer spriteRenderer;
 
+        private void Awake()
+        {
 
+        }
     }
 }

2209a08002dd27ab639b617a7e53ca4d8139fc80
ITR <ITR@live.no>
2020-07-22 03:50:01 +0200

    08:40

--- Assets/Scripts/Game5/Manager.cs
+++ Assets/Scripts/Game5/Manager.cs
@@ -5,7 +5,7 @@ namespace Game5
     public class Manager : MonoBehaviour
     {
         public const int BitsPerSecond = 44100;
-        public const float BeatsPerSecond = 1;
+        public const float BeatsPerSecond = 3;
 
         public static readonly int ClipLength = Mathf.FloorToInt(
             44100 * 0.95f / BeatsPerSecond

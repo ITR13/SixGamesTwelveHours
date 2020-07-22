cd3f012a5c803b2ca1b6103afefcea27695afaab
ITR <ITR@live.no>
2020-07-22 05:39:00 +0200

    10:29

---
+++ Assets/Scripts/Game6/BulletScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}

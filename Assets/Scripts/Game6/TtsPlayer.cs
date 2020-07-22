c653b73bd59312eba5daa0e2dc0baf8b592794bb
ITR <ITR@live.no>
2020-07-22 05:20:00 +0200

    10:10

---
+++ Assets/Scripts/Game6/TtsPlayer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TtsPlayer : MonoBehaviour
+{
+    [SerializeField] private new Camera camera;
+    [SerializeField] private 
+}

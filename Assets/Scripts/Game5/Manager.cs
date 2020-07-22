8e4f095a254d6836cd2c778d3daae665cfcf7312
ITR <ITR@live.no>
2020-07-22 03:42:00 +0200

    08:32

--- Assets/Scripts/Game5/Manager.cs
+++ Assets/Scripts/Game5/Manager.cs
@@ -4,8 +4,13 @@ namespace Game5
 {
     public class Manager : MonoBehaviour
     {
+        public const int BitsPerSecond = 44100;
         public const float BeatsPerSecond = 2;
 
+        public readonly int ClipLength = Mathf.FloorToInt(
+            44100 / BeatsPerSecond
+        );
+        
 
         [SerializeField] private Row[] rows;
 
